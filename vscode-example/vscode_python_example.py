ee2ca7f333e2b1f63fb8f20acaf5c7a4059ddbb2
Sudarshan Koirala <snkoirala45@gmail.com>
2023-06-23 14:22:32 +0000

    vscode example files

---
+++ vscode-example/vscode_python_example.py
@@ -0,0 +1,18 @@
+def test():
+    """this is a test function
+    """
+    pass
+
+
+def test1(x:int, y:int) -> int:
+    """_summary_
+
+    Args:
+        x (int): _description_
+        y (int): _description_
+
+    Returns:
+        int: _description_
+    """
+    z = x + y
+    return z
